8b8c0975b94745b639e2bd7a6b8cf16b7dcf8736
codexmuneer <muneerhanif7@gmail.com>
2025-11-20 20:03:07 +0500

    bugs

--- utils.py
+++ utils.py
@@ -7,11 +7,11 @@ This module provides helper functions used in unit testing.
 def add_numbers(first_value, second_value):
     """
     Performs addition of two numeric values.
-    
+
     Args:
         first_value (int/float): First number to add
         second_value (int/float): Second number to add
-    
+
     Returns:
         int/float: Sum of the two input values
     """
@@ -21,11 +21,11 @@ def add_numbers(first_value, second_value):
 def multiply_numbers(first_value, second_value):
     """
     Performs multiplication of two numeric values.
-    
+
     Args:
         first_value (int/float): First number to multiply
         second_value (int/float): Second number to multiply
-    
+
     Returns:
         int/float: Product of the two input values
     """
